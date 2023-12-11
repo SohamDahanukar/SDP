92fc279d33f94fa016242156e407f1bca4a4f15f
SohamDahanukar <146631306+SohamDahanukar@users.noreply.github.com>
2023-12-11 15:17:26 +0530

    Add files via upload

--- Account.java
+++ Account.java
@@ -1,46 +1,11 @@
 public class Account {
-    private int StudentId = 0;
-    public int getStudentId(int StudentId) {
-        return StudentId;
-    }
-    private String name = "";
-    public String getName(String name) {
-        return name;
-    }
-
-    private double qualifiyingExammarks = 0;
-    public double getQualifiyingExammarks(double qualifiyingExammarks) {
-        return qualifiyingExammarks;
-    }
-
-    private char residentialStatus = '0';
-    public char getresidentialStatus(char residenceStatus) {
-        return residenceStatus;
-    }
-
-    private int YearofEng = 0;
-    public int getYearofEng(int YearofEng) {
-        return YearofEng;
-    }
-
-    public static void main(String[] args)  {
-        Account info = new Account();
-        int getStudentId = info.getStudentId(56);
-        String name = info.getName("Jacob");
-        double qualifiyingExammarks = info.getQualifiyingExammarks(78.7);
-        char residenceStatus = info.getresidentialStatus('F');
-        int yearOfEng = info.getYearofEng(3);
-
-        System.out.println("Student ID:"+getStudentId);
-        System.out.println("Student Name:"+ name);
-        System.out.println("qualifiyingExammarks:"+ qualifiyingExammarks);
-        System.out.println(" residenceStatus:"+ residenceStatus);
-        System.out.println(" yearOfEng:"+ yearOfEng);
-
-
-
-
-    }
+  private double balance = 1000000000000.00;    // State/variable
+  public double getBalance(int accountId) { // Behaviour/method
+    return balance;
+  }
+  public static void main(String[] args) {    // main method
+    Account accnt = new Account();            // Object creation
+    double value = accnt.getBalance(123456);  // Method invocation
+    System.out.println("The balance is: " + value);
+  }
 }
-   
-  
